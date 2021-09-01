c10a71d66e84d863078c4fe5d6e964fe694203ce
milicaz <zm025555@gmail.com>
2021-09-01 15:38:31 +0200

    Klasa korpa

---
+++ FoodOrder/src/beans/Cart.java
@@ -0,0 +1,23 @@
+package beans;
+
+public class Cart {
+
+	//pojedinacan artikal i kolicina
+	//korisnik cija je korpa
+	private int price;
+
+	public Cart() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+}
