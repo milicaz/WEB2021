2abda7709f6d8e335d574420739ee8a9b27cfce2
milicaz <zm025555@gmail.com>
2021-09-10 23:34:04 +0200

    Izmene

--- FoodOrder/WebContent/kPorudzbina.js
+++ FoodOrder/WebContent/kPorudzbina.js
@@ -3,49 +3,62 @@ $(document).ready(function() {
 		url: "rest/users/getLogged",
 		contentType: "application/json",
 		success: function(user) {
-			alert("Role je " + user.role)
-
+			//alert("Role je " + user.role)
 			if (user.role == "kupac") {
+				$("#div_kupac").attr("hidden", false)
 				$.get({
 					url: "rest/orders/allOrders",
 					contentType: "application/json",
 					success: function(orders) {
 						for (let o in orders) {
-
-							if (orders[o].kupacId == user.id) {
-								alert("Usao je u if")
+							const a = []
+							if (orders[o].kupac.id == user.id) {
+								for(let it in orders[o].items){
+									a.push(orders[o].items[it].name)
+								}
+								//alert("Usao je u if")
 								let tr = $('<tr></tr>');
 								let tdId = $('<td>' + orders[o].id + '</td>');
-								let tdArtikli = $('<td>' + orders[o].orderItemId + '</td>');
+								let tdArtikli = $('<td>' + a + '</td>');
 								let tdRestoran = $('<td>' + orders[o].restaurantId + '</td>');
 								let tdDatum = $('<td>' + orders[o].datum + '</td>');
 								let tdCena = $('<td>' + orders[o].price + '</td>');
-								let tdKupac = $('<td>' + orders[o].kupacId + '</td>');
+								let tdKupac = $('<td>' + orders[o].kupac.firstName + " " + orders[o].kupac.firstName + '</td>');
 								let tdStatus = $('<td>' + orders[o].status + '</td>');
 
 								tr.append(tdId).append(tdArtikli).append(tdRestoran).
 									append(tdDatum).append(tdCena).append(tdKupac).append(tdStatus);
-								$('#tabela tbody').append(tr);
+								$('#tabela_kupac tbody').append(tr);
+								
 							}
 						}
 					}
 				})
-			} else if (user.role == "dostavljac") {
+			} if (user.role == "dostavljac") {
 
+				$("#div_porudzbina").attr("hidden", false)
+				$("#div_preuzete").attr("hidden", false)
 				for (let uo in user.orders) {
-					alert("Id porudzbine je: " + user.orders[uo].id)
+					const artikli = []
+					for (let it in user.orders[uo].items) {
+						artikli.push(user.orders[uo].items[it].name)
+
+						//alert("Artikli su : " + user.orders[uo].items[it].name)
+					}
+
 					let tr = $('<tr></tr>');
 					let tdId = $('<td>' + user.orders[uo].id + '</td>');
-					let tdArtikli = $('<td>' + user.orders[uo].orderItemId + '</td>');
+					let tdArtikli = $('<td>' + artikli + '</td>');
 					let tdRestoran = $('<td>' + user.orders[uo].restaurantId + '</td>');
 					let tdDatum = $('<td>' + user.orders[uo].datum + '</td>');
 					let tdCena = $('<td>' + user.orders[uo].price + '</td>');
-					let tdKupac = $('<td>' + user.orders[uo].kupacId + '</td>');
+					let tdKupac = $('<td>' + user.orders[uo].kupac.firstName + " " + user.orders[uo].kupac.lastName + '</td>');
 					let tdStatus = $('<td>' + user.orders[uo].status + '</td>');
 
 					tr.append(tdId).append(tdArtikli).append(tdRestoran).
 						append(tdDatum).append(tdCena).append(tdKupac).append(tdStatus);
 					$('#tabela_preuzete tbody').append(tr);
+
 				}
 
 
@@ -54,16 +67,22 @@ $(document).ready(function() {
 					contentType: "application/json",
 					success: function(orders) {
 						for (let o in orders) {
-
+							//const art = []
 							if (orders[o].status == "ceka dostavljaca") {
-								alert("Usao je u if")
+								const art = []
+								for (let it in orders[o].items) {
+									art.push(orders[o].items[it].name)
+
+									//alert("Artikli su : " + user.orders[uo].items[it].name)
+								}
+								//alert("Usao je u if")
 								let tr = $('<tr></tr>');
 								let tdId = $('<td>' + orders[o].id + '</td>');
-								let tdArtikli = $('<td>' + orders[o].orderItemId + '</td>');
+								let tdArtikli = $('<td>' + art + '</td>');
 								let tdRestoran = $('<td>' + orders[o].restaurantId + '</td>');
 								let tdDatum = $('<td>' + orders[o].datum + '</td>');
 								let tdCena = $('<td>' + orders[o].price + '</td>');
-								let tdKupac = $('<td>' + orders[o].kupacId + '</td>');
+								let tdKupac = $('<td>' + orders[o].kupac.firstName + " " + orders[o].kupac.lastName + '</td>');
 								let tdStatus = $('<td>' + orders[o].status + '</td>');
 
 								tr.append(tdId).append(tdArtikli).append(tdRestoran).
